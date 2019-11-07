b4486d6f85e89b22266d81ab2b6afee8111f92d8
alvesmog <alvesmog@gmail.com>
2019-11-07 19:39:55 -0300

    abstracao do primeiro exercicio da main

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner s = new Scanner(System.in);
+		primeiro_exercicio.restoDivisaoInteiros();
 		
-		int n1, n2, n3;
-		
-		System.out.println("Digite um numero inteiro");
-		
-		n1 = s.nextInt();
-		
-		System.out.println("Digite outro numero inteiro");
-		
-		n2 = s.nextInt();
-		
-		n3 = n1%n2;
-		
-		System.out.println("O resto da divisão de " + n1 + " por " + n2 + " é... " + n3);
-		
-
 	}
 
 }
---
+++ src/primeiro_exercicio.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class primeiro_exercicio {
+	
+	public static void restoDivisaoInteiros() {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int n1, n2, n3;
+		
+		System.out.println("Digite um numero inteiro");
+		
+		n1 = s.nextInt();
+		
+		System.out.println("Digite outro numero inteiro");
+		
+		n2 = s.nextInt();
+		
+		n3 = n1%n2;
+		
+		System.out.println("O resto da divisão de " + n1 + " por " + n2 + " é... " + n3);
+		
+		s.close();
+		
+	}
+
+}
