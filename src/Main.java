f62e6e8a8e8129be6bf74f80143d9cd532c0ddc5
alvesmog <alvesmog@gmail.com>
2019-11-07 20:03:06 -0300

    segundo exercicio: maior entre dois números

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		primeiro_exercicio.restoDivisaoInteiros();
+		Scanner s2 = new Scanner(System.in);
+		
+		System.out.println("Escolha o exercício: \n");
+		
+		System.out.println("1 - Resto divisão inteiros");
+		System.out.println("2 - Maior entre dois números");
+		
+		int opcao = s2.nextInt();
+		
+		switch (opcao) {
+		
+			case 1:
+				primeiro_exercicio.restoDivisaoInteiros();
+				break;
+			case 2:
+				segundo_exercicio.maiorEntreDoisNumeros();
+				break;
+		}
 		
 	}
 
---
+++ src/segundo_exercicio.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class segundo_exercicio {
+	
+	public static void maiorEntreDoisNumeros() {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int n1,n2;
+		
+		System.out.println("Digite o primeiro número inteiro:");
+		
+		n1 = s.nextInt();
+		
+		System.out.println("Digite o segundo número inteiro:");
+		
+		n2 = s.nextInt();
+		
+		if(n1>n2) {
+			System.out.println("O maior entre os dois é: " + n1);			
+		} else if (n2>n1) {
+			System.out.println("O maior entre os dois é: " + n2);
+		} else if (n2==n1) {
+			System.out.println("Os números são iguais!");
+		}
+		
+	}
+
+}
